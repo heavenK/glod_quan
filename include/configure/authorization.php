0c5634810c436d341fecc945179436c32186dc02
heavenK <heaven@we54.com>
2012-10-26 13:26:30 +0800

    init

---
+++ include/configure/authorization.php
@@ -0,0 +1,13 @@
+<?php
+$value = array (
+  1 => 
+  array (
+    0 => 'team',
+    1 => 'help',
+    2 => 'order',
+    3 => 'coupon',
+    4 => 'market',
+    5 => 'admin',
+  ),
+);
+?>
---
+++ uc_client/config.php
@@ -0,0 +1,20 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '');
+define('UC_DBNAME', 'newbbs');
+define('UC_DBCHARSET', 'gbk');
+define('UC_DBTABLEPRE', '`newbbs`.pre_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'gbk');
+define('UC_KEY', 'f82eRtSXSYBKe0hqHBkp2cwzFEPlF1jtrHDYaC8');
+define('UC_API', 'http://bbs-test.we54.com/uc_server');
+define('UC_APPID', '4');
+define('UC_IP', '');
+define('UC_PPP', 20);
+?>
