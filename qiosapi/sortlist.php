16ea10b269c10701f1bd5fe92f131200b7aeafe5
zh <winners122@sina.com>
2012-10-29 15:47:40 +0800

    change for quan

--- qiosapi/sortlist.php
+++ qiosapi/sortlist.php
@@ -1,11 +1,11 @@
 <?php
 require_once(dirname(dirname(__FILE__)) . '/app.php');
 $teams = DB::LimitQuery('team', array(
-	'condition' => $condition,
+	'condition' => array('zone'=>'group'),
 	'order' => 'ORDER BY begin_time DESC, sort_order DESC, id DESC',
 ));
 $categories = DB::LimitQuery('category', array(
-	'condition' => $condition,
+	'condition' => array('zone'=>'partner'),
 	'order' => 'ORDER BY display ASC, sort_order DESC, id DESC',
 ));
 print_r($categories);
