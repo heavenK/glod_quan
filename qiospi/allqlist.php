c1e1ba961604244589d84013bbd91790a127a20e
zh <winners122@sina.com>
2012-10-29 14:41:49 +0800

    change for quan

--- qiospi/allqlist.php
+++ qiospi/allqlist.php
@@ -4,5 +4,11 @@ $teams = DB::LimitQuery('team', array(
 	'condition' => $condition,
 	'order' => 'ORDER BY begin_time DESC, sort_order DESC, id DESC',
 ));
+$categories = DB::LimitQuery('category', array(
+	'condition' => $condition,
+	'order' => 'ORDER BY display ASC, sort_order DESC, id DESC',
+));
+print_r($categories);
+echo("/r/n");
 print_r($teams);
 ?>
