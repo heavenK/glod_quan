24684d2069e0d1508f25dc6b332cf0e030cbd0e3
zh <winners122@sina.com>
2012-10-29 16:05:46 +0800

    change for q

--- qiosapi/sortlist.php
+++ qiosapi/sortlist.php
@@ -1,14 +1,22 @@
 <?php
 require_once(dirname(dirname(__FILE__)) . '/app.php');
-$group = DB::LimitQuery('category', array(
-	'condition' => array('zone'=>'group'),
+$sorts = DB::LimitQuery('category', array(
 	'order' => 'ORDER BY display ASC, sort_order DESC, id DESC',
 ));
-$partner = DB::LimitQuery('category', array(
-	'condition' => array('zone'=>'partner'),
-	'order' => 'ORDER BY display ASC, sort_order DESC, id DESC',
-));
-print_r($group);
-echo("<br>");
-print_r($partner);
+$sort=array('zhonglei'=>array(),'shangquan'=>array());
+$a=1;
+$b=1;
+foreach($sorts as $key=>$value){
+	if($value['zone']=='group'){
+		$sort['zhonglei'][$a]['zhongleiID']=$value['id'];
+		$sort['zhonglei'][$a]['zhongleiName']=$value['name'];
+		$sort['zhonglei'][$a]['imgURL']='';
+		$a++;
+	}else if($value['zone']=='partner'){
+		$sort['shangquan'][$a]['shangquanID']=$value['id'];
+		$sort['shangquan'][$a]['shangquanName']=$value['name'];
+		$b++;
+	}
+}
+echo(json_encode($sort));
 ?>
