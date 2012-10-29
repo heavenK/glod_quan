68e7ce1956b9c7e622f708beec42d98dad6ab727
zh <winners122@sina.com>
2012-10-29 16:47:58 +0800

    change for q

---
+++ qiosapi/svquanlist.php
@@ -0,0 +1,21 @@
+<?php
+if(empty($_REQUEST['certificateID'])&&empty($_REQUEST['certificateType'])){
+	$quan=array('code'=>4);
+	echo(json_encode($quan));
+	exit();
+}
+if(trim($_GET['certificateID'])>0){
+	$qid=trim($_GET['certificateID']);
+}
+$imgtype=trim($_GET['certificateType']);
+if(!empty($qid)){
+	$condition=array('id'=>$qid);
+}else{
+	$condition='';
+}
+$teams = DB::LimitQuery('team', array(
+	'condition' => $condition,
+	'order' => 'ORDER BY begin_time DESC, sort_order DESC, id DESC',
+));
+print_r($teams);
+?>
