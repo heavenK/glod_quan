fce2727dcf211a31a496ea972e0ee943bb19ed4a
zh <winners122@sina.com>
2012-10-30 17:50:32 +0800

    Signed-off-by: zh <winners122@sina.com>

--- qiosapi/nbquanlist.php
+++ qiosapi/nbquanlist.php
@@ -4,5 +4,14 @@ $teams = DB::LimitQuery('partner', array(
 	
 //	'offset' => $offset,
 ));
+
+$teamss = DB::LimitQuery('team', array(
+	'condition' => $condition,
+	'order' => 'ORDER BY begin_time DESC, sort_order DESC, id DESC',
+	'size' => $limit,
+//	'offset' => $offset,
+));
 print_r($teams);
+echo("<br>");
+print_r($teamss); 
 ?>
