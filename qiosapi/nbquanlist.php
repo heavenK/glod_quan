923d0143ee88003e3281c62349872cad08e3ddae
zh <winners122@sina.com>
2012-10-31 10:16:14 +0800

    Signed-off-by: zh <winners122@sina.com>

--- qiosapi/nbquanlist.php
+++ qiosapi/nbquanlist.php
@@ -1,9 +1,20 @@
 <?php
+if(empty($_REQUEST['certificateID'])&&empty($_REQUEST['certificateType'])&&empty($_REQUEST['lon'])&&empty($_REQUEST['lat'])){
+	$quan=array('code'=>4);
+	echo(json_encode($quan));
+	exit();
+}
 require_once(dirname(dirname(__FILE__)) . '/app.php');
+$longlat=trim($_GET['lon']).','.trim($_GET['lat']);
 $teams = DB::LimitQuery('partner', array(
-	
+//	'size' => $limit,
 //	'offset' => $offset,
 ));
+foreach($teams as $key=>$value){
+	list($longi,$lati) = preg_split('/[,\s]+/',$value['longlat'],-1,PREG_SPLIT_NO_EMPTY);
+	echo($longi."--".$lati."<br>");
+}
+
 
 $teamss = DB::LimitQuery('team', array(
 	'condition' => $condition,
