4af881aa2b9384dfa1816b43b8c5de9b9648d78e
zh <winners122@sina.com>
2012-10-31 14:48:33 +0800

    Signed-off-by: zh <winners122@sina.com>

--- qiosapi/serslist.php
+++ qiosapi/serslist.php
@@ -4,12 +4,11 @@ $searchName = isset($_GET['searchTxt'])?trim(strip_tags($_GET['searchTxt'])):'';
 $condition[] = "( title like '%".mysql_escape_string($searchName)."%' )";
 $city_id = abs(intval($city['id']));
 $condition[] = "((city_ids like '%@{$city_id}@%' or city_ids like '%@0@%') or city_id in(0,{$city_id}))";
-$limit=2;
+
 $count = Table::Count('team', $condition);
 $teams = DB::LimitQuery('team', array(
 	'condition' => $condition,
 	'order' => 'ORDER BY begin_time DESC, sort_order DESC, id DESC',
-	'size' => $limit,
 //	'offset' => $offset,
 ));
 $quan=array('code'=>2,'certificates'=>array());
