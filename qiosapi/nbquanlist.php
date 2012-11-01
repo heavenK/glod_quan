ef7542c4b05c3228058344123224818c2cf13b44
zh <winners122@sina.com>
2012-11-01 14:54:18 +0800

    Signed-off-by: zh <winners122@sina.com>

--- qiosapi/nbquanlist.php
+++ qiosapi/nbquanlist.php
@@ -14,25 +14,24 @@ $partners = DB::LimitQuery('partner', array(
 foreach($partners as $key=>$value){
 	$str=array();
 	list($longi,$lati) = preg_split('/[,\s]+/',$value['longlat'],-1,PREG_SPLIT_NO_EMPTY);
-	echo($lon."---".($lon-0.01)."--".($lon+0.01)."<br>");
-	echo($lat."---".($lat-0.01)."--".($lat+0.01)."<br>");
 	if(($longi>=($lon-0.01)&&$longi<($lon+0.05))&&($lati>=($lat-0.01)&&$lati<($lat+0.05))){
 		
 	}else{
 		unset($partners[$key]);
 	}
 } 
+$partner_ids = Utility::GetColumn($partners, 'id');
+$teams = Table::Fetch('team', $partner_ids);
 
 
-
-$teams = DB::LimitQuery('team', array(
+/*$teams = DB::LimitQuery('team', array(
 	'condition' => $condition,
 	'order' => 'ORDER BY begin_time DESC, sort_order DESC, id DESC',
 	'size' => $limit,
 //	'offset' => $offset,
-));
+));*/
 
 print_r($partners);
-/*echo("<br>");
-print_r($teams); */
+echo("<br>");
+print_r($teams); 
 ?>
