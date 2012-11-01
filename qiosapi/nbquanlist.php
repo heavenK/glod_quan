aae93e24436e67c4e8b70a9058f115d1fed715b7
zh <winners122@sina.com>
2012-11-01 13:28:38 +0800

    Signed-off-by: zh <winners122@sina.com>

--- qiosapi/nbquanlist.php
+++ qiosapi/nbquanlist.php
@@ -14,7 +14,7 @@ $partners = DB::LimitQuery('partner', array(
 foreach($partners as $key=>$value){
 	$str=array();
 	list($longi,$lati) = preg_split('/[,\s]+/',$value['longlat'],-1,PREG_SPLIT_NO_EMPTY);
-	if(($longi>($lon-10)&&$longi<($lon+10))&&($lati>($lat-10)&&$lati<($lat+10))){
+	if(($longi>($lon-0.01)&&$longi<($lon+0.01))&&($lati>($lat-0.01)&&$lati<($lat+0.01))){
 		
 	}else{
 		unset($partners[$key]);
