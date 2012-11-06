03476fb72333046236200765d0ad24e2cb65f64f
zh <winners122@sina.com>
2012-11-06 14:32:57 +0800

    Signed-off-by: zh <winners122@sina.com>

---
+++ qiosapi/partnerlist.php
@@ -0,0 +1,14 @@
+<?php
+if(empty($_REQUEST['certificateID'])){
+	$quan=array('code'=>4);
+	echo(json_encode($quan));
+	exit();
+}
+require_once(dirname(dirname(__FILE__)) . '/app.php');
+$qid=isset($_REQUEST['certificateID'])?trim(strip_tags($_REQUEST['certificateID'])):'';
+$condition=array('id'=>$qid);
+$teams = DB::LimitQuery('team', array(
+	'condition' => $condition,
+));
+$partner_ids = Utility::GetColumn($teams, 'partner_id');
+?>
