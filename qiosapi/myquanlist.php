8eb896e160c0f2280dc54a72cff84e7d1a33d9f5
zh <winners122@sina.com>
2012-11-06 16:23:43 +0800

    Signed-off-by: zh <winners122@sina.com>

--- qiosapi/myquanlist.php
+++ qiosapi/myquanlist.php
@@ -50,7 +50,9 @@ foreach($teams as $key=>$value){
 	$sortid=array(0=>$value['group_id']);
 	$type = Table::Fetch('category', $sortid);
 	$str['typeName']=$type[$value['group_id']]['name'];
-	$str['likeCnt']=$value['now_number'];
+	$lcondition=array('team_id'=>$value['team_id']);
+	$count = Table::Count('likecoupon', $lcondition);
+	$str['likeCnt']=$count;
 	$str['content']=$value['summary'];
 	array_push($quan['certificates'],$str);
 }
