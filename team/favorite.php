bd9922a3ae316426330a23f8cd7709ebdd856f24
zh <winners122@sina.com>
2012-11-07 10:32:53 +0800

    Signed-off-by: zh <winners122@sina.com>

---
+++ team/favorite.php
@@ -0,0 +1,10 @@
+<?php
+require_once(dirname(dirname(__FILE__)) . '/app.php');
+$id = abs(intval($_GET['id']));
+$team = Table::Fetch('team', $id);
+if ( !$team || $team['begin_time']>time() ) {
+	Session::Set('error', '团购项目不存在');
+	redirect( WEB_ROOT . '/index.php' );
+}
+echo($login_user_id);
+?>
