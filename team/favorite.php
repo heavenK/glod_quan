f2b5b0211fe1deac5c053655b495a5f2ae6d534f
zh <winners122@sina.com>
2012-11-08 09:59:15 +0800

    Signed-off-by: zh <winners122@sina.com>

--- team/favorite.php
+++ team/favorite.php
@@ -12,6 +12,17 @@ $data['user_name']=$login_user['username'];
 $data['partner_id']=$team['partner_id'];
 $data['team_id']=$team['id'];
 $data['create_time']=time();
+$condition=array('user_id'=>$login_user_id,
+				'user_name'=>$login_user['username'],
+				'team_id'=>$team['id']
+				);
+$favor=DB::LimitQuery('likecoupon', array(
+	'condition' => $condition,
+));
+if(!empty($favor)){
+	Session::Set('error', '金券已收藏！');
+	redirect( WEB_ROOT . "/team.php?id={$id}");
+}
 $insertid=DB::Insert('likecoupon', $data);
 Session::Set('notice', "收藏成功！");
 redirect( WEB_ROOT . "/team.php?id={$id}");
