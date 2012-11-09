6310db84b1b188aa1e626d67b83f86bd8fb734e3
zh <winners122@sina.com>
2012-11-09 16:40:46 +0800

    Signed-off-by: zh <winners122@sina.com>

--- qiosapi/partnerlist.php
+++ qiosapi/partnerlist.php
@@ -42,6 +42,11 @@ foreach($partner as $key=>$value){
 	$partner_arr['sellerTel']=$value['phone'];
 	$partner_arr['cerContent']=$teams[0]['summary'];
 	$partner_arr['cerValidityDate']=date("Y.m.d",$teams[0]['end_time']);
+	if($teams[0]['delivery']=='voucher'){
+		$partner_arr['cerDelivery']='picture';
+	}else if($teams[0]['delivery']=='express'){
+		$partner_arr['cerDelivery']='Mobile';
+	}
 	$partner_arr['sellerContent']=$value['location'];
 	$partner_arr['imgURLs']=array(array('imgURL'=>$value['image1']),array('imgURL'=>$value['image2']));
 }
