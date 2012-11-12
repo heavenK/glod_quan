a92cedfc72630b493de7553d33f05394ed317568
zh <winners122@sina.com>
2012-11-12 14:45:18 +0800

    Signed-off-by: zh <winners122@sina.com>

--- qiosapi/freegetquan.php
+++ qiosapi/freegetquan.php
@@ -1,7 +1,26 @@
 <?php
 require_once(dirname(dirname(__FILE__)) . '/app.php');
 if(isset($_REQUEST['type'])&&$_REQUEST['type']=='M'){
-	echo sms_send(1391234567,'°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡°¡');
+	$user_id=isset($_REQUEST['userId'])?trim(strip_tags($_REQUEST['userId'])):'';
+	$user_name=isset($_REQUEST['userName'])?trim(strip_tags($_REQUEST['userName'])):'';
+	$qid=isset($_REQUEST['quanId'])?trim(strip_tags($_REQUEST['quanId'])):'';
+	$phone=isset($_REQUEST['phone'])?trim(strip_tags($_REQUEST['quanId'])):'';
+	$id = abs(intval($qid));
+	$team = Table::Fetch('team', $id);
+	if ( !$team || $team['begin_time']>time() ) {
+		$back=array("err"=>1);
+		echo(json_encode($back));
+		exit();
+	}
+	$data['user_id']=$user_id;
+	$data['user_name']=$user_name;
+	$data['partner_id']=$team['partner_id'];
+	$data['team_id']=$team['id'];
+	$data['mobile']=$phone;
+	$data['content']='²âÊÔ¶ÌÐÅ£¬ÓÅ»ÝÈ¯´úÂëtest123456789,¸ÐÐ»²ÎÓë²âÊÔ¡£';
+	$data['create_time']=time();
+	$res=sms_send($phone,$data['content']);
+echo($res);
 }else if(isset($_REQUEST['type'])&&$_REQUEST['type']=='P'){
 	$user_id=isset($_REQUEST['userId'])?trim(strip_tags($_REQUEST['userId'])):'';
 	$user_name=isset($_REQUEST['userName'])?trim(strip_tags($_REQUEST['userName'])):'';
@@ -17,6 +36,7 @@ if(isset($_REQUEST['type'])&&$_REQUEST['type']=='M'){
 	$data['user_name']=$user_name;
 	$data['partner_id']=$team['partner_id'];
 	$data['team_id']=$team['id'];
+	$data['state']=1;
 	$data['create_time']=time();
 	if(empty($team['qimage'])){
 		$back=array('err'=>2);
