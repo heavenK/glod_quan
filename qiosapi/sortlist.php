5f87716748acc699285352d633e44181024afb13
zh <winners122@sina.com>
2012-11-13 14:44:18 +0800

    Signed-off-by: zh <winners122@sina.com>

--- qiosapi/sortlist.php
+++ qiosapi/sortlist.php
@@ -17,15 +17,15 @@ $sort=array('code'=>2,'zhonglei'=>array(),'shangquan'=>array());
 
 foreach($group as $key=>$value){
 	$str=array();
-	$str['zhongleiID']=$value['id'];
-	$str['zhongleiName']=$value['name'];
+	$str['zhongleiID']=empty($value['id'])?0:$value['id'];
+	$str['zhongleiName']=empty($value['name'])?0:$value['name'];
 	$str['imgURL']='';
 	array_push($sort['zhonglei'],$str);
 }
 foreach($partner as $key=>$value){
 	$pstr=array();
-	$pstr['shangquanID']=$value['id'];
-	$pstr['shangquanName']=$value['name'];
+	$pstr['shangquanID']=empty($value['id'])?0:$value['id'];
+	$pstr['shangquanName']=empty($value['name'])?0:$value['name'];
 	array_push($sort['shangquan'],$pstr);
 }
 echo(json_encode($sort));
