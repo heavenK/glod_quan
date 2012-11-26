8a72d2fc97785f69d7e50e43409d135be3dbceff
zh <winners122@sina.com>
2012-11-26 14:51:46 +0800

    Signed-off-by: zh <winners122@sina.com>

--- manage/partner/create.php
+++ manage/partner/create.php
@@ -17,6 +17,16 @@ if ( $_POST ) {
 	$table->image = upload_image('upload_image', null, 'team', true);
 	$table->image1 = upload_image('upload_image1', null, 'team');
 	$table->image2 = upload_image('upload_image2', null, 'team');
+	$table->image3 = upload_image('upload_image3', null, 'team');
+	$table->image4 = upload_image('upload_image4', null, 'team');
+	$table->image5 = upload_image('upload_image5', null, 'team');
+	$table->image6 = upload_image('upload_image6', null, 'team');
+	$table->image7 = upload_image('upload_image7', null, 'team');
+	$table->image8 = upload_image('upload_image8', null, 'team');
+	$table->image9 = upload_image('upload_image9', null, 'team');
+	$table->image10 = upload_image('upload_image10', null, 'team');
+	$table->image11 = upload_image('upload_image11', null, 'team');
+	$table->image12 = upload_image('upload_image12', null, 'team');
 	$table->insert(array(
 		'username', 'user_id', 'city_id', 'title', 'group_id',
 		'bank_name', 'bank_user', 'bank_no', 'create_time',
